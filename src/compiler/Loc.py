ef725c0dea6df0c783b64186e34056a88e43babc
Elias Toukolehto <elias.toukolehto@helsinki.fi>
2026-01-25 20:46:37 +0200

    add test location objet

--- src/compiler/Loc.py
+++ src/compiler/Loc.py
@@ -2,5 +2,18 @@ from dataclasses import dataclass
 
 @dataclass
 class Loc:
-  line: int
-  col: int
+  def __init__(self, line: int, col: int, test_object: bool =False):
+    self.line = line
+    self.col = col
+    self.test_object = test_object
+
+  def __eq__(self, other: object) -> bool:
+    if not isinstance(other, Loc):
+      return False
+
+    if self.test_object or other.test_object:
+      return True
+
+    return self.line == other.line and self.col == other.col
+
+L = Loc(0,0,True)
--- tests/tokenizer_test.py
+++ tests/tokenizer_test.py
@@ -1,8 +1,13 @@
 from compiler.tokenizer import tokenize
-from compiler.Loc import Loc
+from compiler.Loc import L, Loc
 from compiler.Token import Token
 
 def test_tokenizer_basics() -> None:
-  assert tokenize("if 3\nwhile") == [Token(loc=Loc(0,0), type="identifier", text="if"),
-                                     Token(loc=Loc(0,3), type="int_literal", text="3"),
-                                     Token(loc=Loc(1,0), type="identifier", text="while")]
+  assert tokenize("if 3\nwhile") == [Token(L, type="identifier", text="if"),
+                                     Token(L, type="int_literal", text="3"),
+                                     Token(L, type="identifier", text="while")]
+
+def test_tokenizer_loc() -> None:
+  assert tokenize("if 3\nwhile") == [Token(Loc(0,0), type="identifier", text="if"),
+                                     Token(Loc(0,3), type="int_literal", text="3"),
+                                     Token(Loc(1,0), type="identifier", text="while")]

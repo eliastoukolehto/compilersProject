11b063fcf825afb11a31c42c9c8a1c91580c42b2
Elias Toukolehto <elias.toukolehto@helsinki.fi>
2026-01-28 17:46:43 +0200

    finish tokenizer

--- src/compiler/tokenizer.py
+++ src/compiler/tokenizer.py
@@ -1,15 +1,27 @@
 import re
-from re import Pattern
+from re import Pattern, Match
 from compiler.Loc import Loc
 from compiler.Token import Token
 
 identifier_r = re.compile(r"[a-zA-Z_][a-zA-Z_0-9]*")
 int_literal_r = re.compile(r"[0-9]+")
+operator_r = re.compile(r"==|<=|>=|!=|>|<|-|\+|\*|\/|%|=")
+punctuation_r = re.compile(r"\(|\)|{|}|,|;")
+
 
 newline_r = re.compile(r"\n")
+comment_r = re.compile(r"\/\/.*|#.*")
 
-regexes: list[Pattern[str]] = [identifier_r, int_literal_r]
-types = ['identifier', 'int_literal']
+regexes: list[Pattern[str]] = [identifier_r, int_literal_r, operator_r, punctuation_r]
+types = ['identifier', 'int_literal', 'operator', 'punctuation']
+
+
+def find_token(source_code: str, i: int) -> tuple[str, str] | None:
+  for j, t_type in enumerate(types):
+    match = regexes[j].match(source_code, i)
+    if match:
+      return (t_type, match[0])
+  return None
 
 
 def tokenize(source_code: str) -> list:
@@ -18,18 +30,22 @@ def tokenize(source_code: str) -> list:
   token_list = []
   i = 0
   while i < len(source_code):
-    for j, t_type in enumerate(types):
-      match = regexes[j].match(source_code, i)
-      if match:
-        token_list.append(Token(Loc(line, col), t_type, match[0]))
-        col += len(match[0])
-        i += len(match[0])
-        break
+    comment_match = comment_r.match(source_code, i)
+    if comment_match is not None:
+      i += len(comment_match[0])
+      continue
     if newline_r.match(source_code, i) is not None:
       line += 1
       col = 0
-    else:
-      col += 1
+      i += 1
+      continue
+    token = find_token(source_code, i)
+    if token:
+      token_list.append(Token(Loc(line, col), token[0], token[1]))
+      col += len(token[1])
+      i += len(token[1])
+      continue
+
+    col += 1
     i += 1
-    continue
   return token_list
--- tests/tokenizer_test.py
+++ tests/tokenizer_test.py
@@ -11,3 +11,26 @@ def test_tokenizer_loc() -> None:
   assert tokenize("if 3\nwhile") == [Token(Loc(0,0), type="identifier", text="if"),
                                      Token(Loc(0,3), type="int_literal", text="3"),
                                      Token(Loc(1,0), type="identifier", text="while")]
+
+def test_tokenizer_op() -> None:
+  assert tokenize("3 == 4 + 7") == [Token(L, type="int_literal", text="3"),
+                                     Token(L, type="operator", text="=="),
+                                     Token(L, type="int_literal", text="4"),
+                                     Token(L, type="operator", text="+"),
+                                     Token(L, type="int_literal", text="7")]
+
+def test_tokenizer_punc() -> None:
+  assert tokenize("{3,4,(7)}") == [Token(L, type="punctuation", text="{"),
+                                     Token(L, type="int_literal", text="3"),
+                                     Token(L, type="punctuation", text=","),
+                                     Token(L, type="int_literal", text="4"),
+                                     Token(L, type="punctuation", text=","),
+                                     Token(L, type="punctuation", text="("),
+                                     Token(L, type="int_literal", text="7"),
+                                     Token(L, type="punctuation", text=")"),
+                                     Token(L, type="punctuation", text="}")]
+  
+def test_tokenizer_comment_loc() -> None:
+  assert tokenize("3 #comment\n4//comment 5\n#6\n7") == [Token(Loc(0,0), type="int_literal", text="3"),
+                                     Token(Loc(1,0), type="int_literal", text="4"),
+                                     Token(Loc(3,0), type="int_literal", text="7")]

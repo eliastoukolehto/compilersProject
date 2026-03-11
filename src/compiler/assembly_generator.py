3d01d8789b44cbfb993eacc499601662ba0d1cf4
Elias Toukolehto <elias.toukolehto@helsinki.fi>
2026-03-12 00:15:17 +0200

    assembly generator task 2

--- src/compiler/assembly_generator.py
+++ src/compiler/assembly_generator.py
@@ -1,4 +1,6 @@
+from dataclasses import fields
 from compiler import ir
+
 
 class Locals:
   """Knows the memory location of every local variable."""
@@ -21,3 +23,83 @@ class Locals:
   def stack_used(self) -> int:
     """Returns the number of bytes of stack space needed for the local variables."""
     return self._stack_used
+
+
+
+def get_all_ir_variables(instructions: list[ir.Instruction]) -> list[ir.IRVar]:
+  result_list: list[ir.IRVar] = []
+  result_set: set[ir.IRVar] = set()
+
+  def add(v: ir.IRVar) -> None:
+    if v not in result_set:
+      result_list.append(v)
+      result_set.add(v)
+
+  for insn in instructions:
+    for field in fields(insn):
+      value = getattr(insn, field.name)
+      if isinstance(value, ir.IRVar):
+        add(value)
+      elif isinstance(value, list):
+        for v in value:
+          if isinstance(v, ir.IRVar):
+            add(v)
+  return result_list
+
+
+
+def generate_assembly(instructions: list[ir.Instruction]) -> str:
+  lines = []
+  def emit(line: str) -> None: lines.append(line)
+
+  locals = Locals(
+    variables=get_all_ir_variables(instructions)
+  )
+
+    # ... Emit initial declarations and stack setup here ...
+
+  for insn in instructions:
+    emit('# ' + str(insn))
+    match insn:
+      case ir.Label():
+        emit("")
+        # ".L" prefix marks the symbol as "private".
+        # This makes GDB backtraces look nicer too:
+        # https://stackoverflow.com/a/26065570/965979
+        emit(f'.L{insn.name}:')
+      case ir.LoadIntConst():
+        if -2**31 <= insn.value < 2**31:
+          emit(f'movq ${insn.value}, {locals.get_ref(insn.dest)}')
+        else:
+          # Due to a quirk of x86-64, we must use
+          # a different instruction for large integers.
+          # It can only write to a register,
+          # not a memory location, so we use %rax
+          # as a temporary.
+          emit(f'movabsq ${insn.value}, %rax')
+          emit(f'movq %rax, {locals.get_ref(insn.dest)}')
+      case ir.Jump():
+        emit(f'jmp .L{insn.label.name}')
+
+      case ir.LoadBoolConst():
+        if insn.value is True:
+          emit(f'movq $1, {locals.get_ref(insn.dest)}')
+        else:
+          emit(f'movq $0, {locals.get_ref(insn.dest)}')
+
+      case ir.Copy():
+        emit(f'movq {locals.get_ref(insn.source)}, %rax')
+        emit(f'movq %rax, {locals.get_ref(insn.dest)}')
+
+      case ir.CondJump():
+        emit(f'cmpq $0, {locals.get_ref(insn.cond)}')
+        emit(f'jne .L{insn.then_label.name}')
+        emit(f'jmp .L{insn.else_label.name}')
+
+  #restore stack
+  emit('movq $0, %rax')
+  emit('movq %rbp, %rsp')
+  emit('popq %rbp')
+  emit('ret')
+
+  return '\n'.join(lines)
